32104d66fa8bfcf38f4134c1713983610e638d8b
Andres Santacruz <andres_v10@live.com>
2021-10-25 22:32:23 -0500

    [ADD]: add Component Footer to layout

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import styles from "../styles/Home.module.css";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href="https://www.instagram.com/vanidosasepakeratinas/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        @vanidosasepakeratinas
+      </a>
+    </footer>
+  );
+};
